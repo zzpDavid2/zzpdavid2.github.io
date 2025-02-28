a36a7821a97afbb4eeb34153f1f808d4a1f7c68a
zzpDavid2 <51421093+zzpDavid2@users.noreply.github.com>
2025-02-27 22:53:27 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://zzpdavid2.github.io',
-    base: '/zzpdavid2.github.io', 
+    // base: 'zzpdavid2.github.io', 
 });
